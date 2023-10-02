3d8802211f230a465389b0deb6a42323697bc8ac
KodeUniverse <dakshin@gmail.com>
2023-10-02 19:20:07 -0400

    Fixed heatmap annotation error by upgrading to seaborn 0.13.0, added .gitignore

--- data_clean_visualize.py
+++ data_clean_visualize.py
@@ -6,6 +6,19 @@ import warnings
 warnings.filterwarnings("ignore")
 from sklearn.preprocessing import MinMaxScaler
 
+
+def preprocess(df):
+
+    df.ffill(inplace=True)
+    alone_col = np.where(df["SibSp"] + df["Parch"] > 0, 1, 0)
+    df.drop(["PassengerId","Name", "Ticket", "Cabin", "SibSp", "Parch"], axis=1, inplace=True)
+    df["TraveledAlone"] = alone_col
+    df = pd.get_dummies(df)
+    
+    colnames = list(df.columns)
+    return pd.DataFrame(df, columns=colnames)
+
+
 train = pd.read_csv("data/train.csv")
 test = pd.read_csv("data/test.csv")
 
@@ -30,31 +43,24 @@ sns.histplot(train, x="Age", kde=True, ax=axes[1][0])
 
 sns.countplot(train, x="Survived", hue="Sex", ax=axes[1][1])
 
-sns.histplot(train, x="Fare", kde=True, ax=axes[1][2])
+
+train = preprocess(train)
+sns.countplot(train, x="TraveledAlone", ax=axes[1][2]) # Feature TraveledAlone is only created after preprocess() call
 
 fig.savefig("output/feature-dist.png")
 
 # One-hot encoding on categorical variables
-train = train.drop(["Name", "Cabin", "Ticket"], axis=1)
-train = pd.get_dummies(train)
+#train = train.drop(["Name", "Cabin", "Ticket"], axis=1)
+#train = pd.get_dummies(train)
 heat_fig, heat_axes = plt.figure(), plt.axes()
 
 # Feature correlation plot
-heatmap = sns.heatmap(train.corr(), annot=train.corr(), fmt='.2f', ax=heat_axes)
+heatmap = sns.heatmap(train.corr(), annot=np.array(train.corr()), fmt='.2f', cmap='hot', ax=heat_axes)
 heat_fig = heatmap.get_figure()
 heat_fig.set_size_inches((10,10))
 heat_fig.savefig("output/feature-corr.png", dpi=400)
+print(np.array(train.corr()))
 
-def preprocess(df):
-
-    df.ffill(inplace=True)
-    alone_col = np.where(df["SibSp"] + df["Parch"] > 0, 1, 0)
-    df.drop(["PassengerId","Name", "Ticket", "Cabin", "SibSp", "Parch"], axis=1, inplace=True)
-    df["TraveledAlone"] = alone_col
-    df = pd.get_dummies(df)
-    
-    colnames = list(df.columns)
-    return pd.DataFrame(df, columns=colnames)
 
 
 
